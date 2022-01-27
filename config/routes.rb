9a5d6498737113dfc9498d1c8c61aabc9af52ded
sknt0730 <zm19568@josai.ac.jp>
2022-01-27 18:36:36 +0000

    user controller and model created

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'about' => "static_pages#about"
   get 'contact' => "static_pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'hello-world' => "foo#hello"
+  get 'signup' => "users#new"
 end
